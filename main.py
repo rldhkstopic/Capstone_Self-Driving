007f4e7475cc58cda5d09fee1eac63ed9dc3d799
rldhkstopic <rldhks6899@naver.com>
2019-05-29 02:59:59 +0900

    sleeping detection

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from shapely.geometry import Polygon, Point
 from moviepy.editor import VideoFileClip
 from scipy.misc import imresize
 from PIL import Image
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
 
 import numpy as np
 import os
@@ -16,6 +19,8 @@ import io
 import cv2
 import pafy
 import pickle
+import dlib
+import imutils
 import argparse
 import time
 import serial
@@ -33,6 +38,7 @@ dark = (1, 1, 1)
 cyan = (255, 255, 0)
 magenta = (255, 0, 255)
 lime = (0, 255, 128)
+purple = (255, 0, 255)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 font2 = cv2.FONT_HERSHEY_PLAIN
@@ -42,7 +48,7 @@ l_pos, r_pos, l_cent, r_cent = 0, 0, 0, 0
 uxhalf, uyhalf, dxhalf, dyhalf = 0, 0, 0, 0
 l_center, r_center, lane_center = ((0, 0)), ((0, 0)), ((0, 0))
 next_frame = (0, 0, 0, 0, 0, 0, 0, 0)
-
+control_flag = False
 
 def arg_parse():
     parses = argparse.ArgumentParser(description='My capstone Design 2019')
@@ -52,6 +58,7 @@ def arg_parse():
     parses.add_argument("--url", dest = 'url', default = False, type = str, help="youtube url link")
     parses.add_argument("--com", dest = 'com', default = False, help = "Setting Arduino port", type = str)
     parses.add_argument("--brate", dest = 'brate', default = 9600, help = "Setting Arduino baudrate")
+    parses.add_argument("--t", dest = 'threshold', default = 0.2, help = "Threshold to determine closed eye")
     return parses.parse_args()
 
 """
@@ -66,6 +73,14 @@ def save_video(filename, frame=60.0):
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     out = cv2.VideoWriter(filename, fourcc, frame, (1280,720))
     return out
+
+""" Eye Detection Ratio """
+def eye_aspect_ratio(eye):
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+	C = dist.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
 
 def grayscale(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -254,8 +269,7 @@ def warning_text(image):
 
     # cv2.putText(image, 'angle = {0}'.format(angle), (10, hei*4), font, 0.7, white, font_size)
     # cv2.putText(image, 'value = {0}'.format(value), (10, hei*5), font, 0.7, white, font_size)
-
-    if mcu_port:
+    if mcu_port and control_flag:
         mcu.write([value])
 
 """ 현재 영상 프레임 표시 """
@@ -409,19 +423,22 @@ def write(x, results, color = [126, 232, 229], font_color = red): # x = output
 
                 if cls == 0:
                     cv2.rectangle(image, c1, c2, blue, 1)
+
                     t_size = cv2. getTextSize(label, font2, 1, 1)[0]
                     c2 = c1[0] + t_size[0] + 3, c1[1] - t_size[1] - 4
-                    # cv2.rectangle(image, c1, c2, white, -1)
-                    cv2.putText(image, label, (c1[0], c1[1] - t_size[1] + 4), font2, 1, blue, 1)
+
+                    cv2.rectangle(image, c1, (c2[0]-3, c2[1]+4), blue, -1)
+                    cv2.putText(image, label, (c1[0], c1[1] - t_size[1] + 10), font2, 1, white, 1)
 
                 else:
                     cv2.rectangle(image, c1, c2, red, 1) # 자동차 감지한 사각형
-                    # cv2.circle(image, (centx, centy), 3, blue, -1) # Detected vehicles' center
 
                     t_size = cv2.getTextSize(label, font2, 1, 1)[0]
                     c2 = c1[0] + t_size[0] + 3, c1[1] - t_size[1] - 4
+
                     cv2.rectangle(image, c1, (c2[0]-3, c2[1]+4), red, -1)
                     cv2.putText(image, label, (c1[0], c1[1] - t_size[1] + 10), font2, 1, white, 1)
+
     return image
 
 def laneregion(image):
@@ -446,6 +463,7 @@ names = "data/coco.names"
 video_directory = "test_videos/"
 video = video_directory + args.video
 
+EYE_AR_THRESH = args['threshold']
 # URL = "https://youtu.be/jieP4QkVze8"
 # URL = "https://youtu.be/YsPdvvixYfo" roi = 1
 url = args.url
@@ -466,14 +484,19 @@ resol = 416 # 해상도
 whalf, height = 640, 720
 
 num_classes = 12
-print("Reading configure file")
+print("[INFO]Reading configure file")
 model = Darknet(cfg)
-print("Reading weights file")
+print("[INFO]Reading weights file")
 model.load_weights(weights)
-print("Reading classes file")
+print("[INFO]Reading classes file")
 classes = load_classes(names)
 set_requires_grad(model, False)
-print("\nNetwork successfully loaded!")
+print("\n[INFO]Network successfully loaded!")
+
+print("[INFO] loading facial landmark predictor...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor.dat")
+print("\n[INFO] Suceess!")
 
 mcu_port = args.com
 mcu_brate = args.brate # Baud rate
@@ -500,109 +523,155 @@ start = time.time()
 
 if url:
     cap = cv2.VideoCapture(play.url)
-else: # python main.py --com COM4 --youtub
+else: # python main.py --com COM4 --youtube
     cap = cv2.VideoCapture(video)
-print("\nVideo is now ready to show.")
+cam = cv2.VideoCapture(0)
+print("\n[INFO]Video and Camera is now ready to show.")
 
 while (cap.isOpened()):
     ret, frame = cap.read()
-    cpframe = frame.copy() # Lane frame copy
+    ret1, frame1 = cam.read()
     if ret:
         # zerof = laneregion(frame)
         # show = cv2.addWeighted(frame, 1, zerof, 0.6, 0)
+        width, height = frame.shape[:2]
 
-        # cv2.rectangle(frame, (0,0), (300, 130), dark, -1)
-        # show_fps(frame, frames, start, color = yellow)
-        # warning_text(frame)
+        cv2.rectangle(frame, (0,0), (300, 130), dark, -1)
+        show_fps(frame, frames, start, color = yellow)
+        warning_text(frame)
 
-        """ Lane Detection """
+        """-------------------------- Eye Detection -------------------------"""
+        global control_flag
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        for rect in detector(gray, 0):
+    		shape = face_utils.shape_to_np(predictor(gray, rect))
+
+    		leftEye = shape[lStart:lEnd]
+    		leftEAR = eye_aspect_ratio(leftEye)
+    		rightEye = shape[rStart:rEnd]
+    		rightEAR = eye_aspect_ratio(rightEye)
+
+    		ear = (leftEAR + rightEAR) / 2.0
+
+    		cv2.drawContours(frame1, [cv2.convexHull(leftEye)], -1, (0, 255, 0), 1)
+    		cv2.drawContours(frame1, [cv2.convexHull(rightEye)], -1, (0, 255, 0), 1)
+
+    		# Threshold만큼 눈이 감기면 Blink로 간주
+    		if ear < EYE_AR_THRESH:
+    			COUNTER += 1
+                if COUNTER > 100:
+                    cv2.putText(frame, "Driver is sleeping", (width-170, 80), font, 1, (0, 0, 255), 2)
+                    cv2.putText(frame, "Now Turn Autonomous Mode", (width-300, 100), font, 1, (0, 0, 255), 2)
+                    control_flag = True
+            else:
+    			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+    				TOTAL += 1
+                    control_flag = False
+    			COUNTER = 0
+
+    		cv2.putText(frame1, "Blinks: {}".format(TOTAL), (10, 30),
+        		font, 0.7, (0, 0, 255), 2)
+    		cv2.putText(frame1, "EAR: {:.2f}".format(ear), (300, 30),
+        		font, 0.7, (0, 0, 255), 2)
+
+        """------------------------- Lane Detection -------------------------"""
+        cpframe = frame.copy() # Lane frame copy
         prc_img, hough = process_image(cpframe)
         lane_detection = visualize(prc_img, args.roi)
 
         """ Object Detection """
-        # if frames %3 == 0: # Frame 높히기; 눈속임
-        #     prep_frame = prep_image(frame, input_dim)
-        #     frame_dim = frame.shape[1], frame.shape[0]
-        #     frame_dim = torch.FloatTensor(frame_dim).repeat(1, 2)
-        #
-        #     if CUDA:
-        #         frame_dim = frame_dim.cuda()
-        #         prep_frame = prep_frame.cuda()
-        #
-        #         with torch.no_grad():
-        #             output = model(Variable(prep_frame, True), CUDA)
-        #             output = write_results(output, confidence, num_classes, nms_thesh)
-        #
-        #             frame_dim = frame_dim.repeat(output.size(0), 1)
-        #     # scaling_factor = torch.min(416/frame_dim, 1)[0].view(-1,   1)
-        #     scaling_factor = torch.min(resol/frame_dim, 1)[0].view(-1, 1)
-        #
-        #     output[:, [1, 3]] -= (input_dim - scaling_factor * frame_dim[:, 0].view(-1, 1))/2
-        #     output[:, [2, 4]] -= (input_dim - scaling_factor * frame_dim[:, 1].view(-1, 1))/2
-        #     output[:, 1:5] /= scaling_factor
-        #
-        #     for i in range(output.shape[0]):
-        #         output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, frame_dim[i,0])
-        #         output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, frame_dim[i,1])
-        #
-        #     zero_frame = np.zeros_like(frame) # Object frame zero copy
-        #     list(write(x, zero_frame) for x in output) # list(map(lambda x: write(x, frame), output))
-        #
-        #     crossx, crossy = lane_cross_point()
-        #
-        #     l_poly = Polygon([(next_frame[0], next_frame[1]), (crossx, crossy), (crossx, 0), (0, 0), (0, 720)])
-        #     r_poly = Polygon([(next_frame[6], next_frame[7]), (crossx, crossy), (crossx, 0), (1280, 0), (1280, 720)])
-        #     c_poly = Polygon([(next_frame[0], next_frame[1]), (crossx, crossy), (next_frame[6], next_frame[7])]) # Center Polygon
-        #
-        #     cnt = 0 # Car count
-        #     vals = [2, 3, 5, 7]
-        #     l_cnt, r_cnt, c_cnt = 0, 0, 0
-        #     for x in output:
-        #         c1 = tuple(x[1:3].int())
-        #         c2 = tuple(x[3:5].int())
-        #         centx = int((c1[0]+c2[0])/2)
-        #         centy = int((c1[1]+c2[1])/2)
-        #
-        #         carbox = Polygon([(c1[0], c1[0]), (c1[0], c1[1]), (c1[1], c1[1]), (c1[1], c1[0])])
-        #         carcent = Point((centx, centy)) # Car Center point
-        #         carundcent = Point((centx, c2[1]))
-        #
-        #         """ 차의 중앙 지점과 겹치는 곳이 있으면 그곳이 차의 위치 """
-        #         for val in vals:
-        #             if int(x[-1]) == val:
-        #                 cnt += 1
-        #                 if l_poly.intersects(carcent):
-        #                     l_cnt += 1
-        #                 if r_poly.intersects(carcent):
-        #                     r_cnt += 1
-        #                 if c_poly.intersects(carcent):
-        #                     c_cnt += 1
-        #                     if c_cnt > 1 : c_cnt = 1
-        #
-        #                     # 앞 차량과의 거리계산
-        #                     pl = carundcent.distance(Point(whalf-5, 720))
-        #                     dist = (pl * 1.8 / (next_frame[6] - next_frame[2])) * 180/np.pi
-        #                     dist = round(map(dist, 20, 40, 10, 70), 2)
-        #
-        #                     cv2.line(frame, (centx, c1[1]), (centx, c1[1]-120), red, 1)
-        #                     cv2.line(frame, (centx-50, c1[1]-120), (centx+40, c1[1]-120), red, 1)
-        #                     cv2.putText(frame, "{} m".format(dist), (centx-45, c1[1]-130), font, 0.6, red, 1)
-        #
-        #                 if l_cnt or r_cnt or c_cnt:
-        #                     cnt = l_cnt + c_cnt + r_cnt
-        #
-        #     cv2.putText(frame, 'vehicles counting : {}'.format(cnt), (10, 75), font, 0.8, white, 1)
-        #     cv2.putText(frame, 'L = {0} / F = {2} / R = {1}'.format(l_cnt, r_cnt, c_cnt), (10, 100), font, 0.7, white, font_size)
-        #
-        #     object_detection = cv2.add(frame, zero_frame)
-        #
-        #     lane_detection = cv2.addWeighted(object_detection, 1, lane_detection, 0.5, 0)
-        lane_detection = cv2.addWeighted(frame, 1, lane_detection, 0.5, 0)
+        if frames %3 == 0: # Frame 높히기; 눈속임
+            prep_frame = prep_image(frame, input_dim)
+            frame_dim = frame.shape[1], frame.shape[0]
+            frame_dim = torch.FloatTensor(frame_dim).repeat(1, 2)
 
-            # cv2.imshow("Result", lane_detection)
-        cv2.imshow("hough", lane_detection)
+            if CUDA:
+                frame_dim = frame_dim.cuda()
+                prep_frame = prep_frame.cuda()
+
+                with torch.no_grad():
+                    output = model(Variable(prep_frame, True), CUDA)
+                    output = write_results(output, confidence, num_classes, nms_thesh)
+
+                    frame_dim = frame_dim.repeat(output.size(0), 1)
+            scaling_factor = torch.min(resol/frame_dim, 1)[0].view(-1, 1)
+
+            output[:, [1, 3]] -= (input_dim - scaling_factor * frame_dim[:, 0].view(-1, 1))/2
+            output[:, [2, 4]] -= (input_dim - scaling_factor * frame_dim[:, 1].view(-1, 1))/2
+            output[:, 1:5] /= scaling_factor
+
+            for i in range(output.shape[0]):
+                output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, frame_dim[i,0])
+                output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, frame_dim[i,1])
+
+            zero_frame = np.zeros_like(frame) # Object frame zero copy
+            list(write(x, zero_frame) for x in output) # list(map(lambda x: write(x, frame), output))
+
+            crossx, crossy = lane_cross_point()
+
+            l_poly = Polygon([(next_frame[0], next_frame[1]), (crossx, crossy), (crossx, 0), (0, 0), (0, 720)])
+            r_poly = Polygon([(next_frame[6], next_frame[7]), (crossx, crossy), (crossx, 0), (1280, 0), (1280, 720)])
+            c_poly = Polygon([(next_frame[0], next_frame[1]), (crossx, crossy), (next_frame[6], next_frame[7])]) # Center Polygon
+
+            cnt = 0 # Car count
+            vals = [2, 3, 5, 7]
+            l_cnt, r_cnt, c_cnt = 0, 0, 0
+            for x in output:
+                c1 = tuple(x[1:3].int())
+                c2 = tuple(x[3:5].int())
+                centx = int((c1[0]+c2[0])/2)
+                centy = int((c1[1]+c2[1])/2)
+                label = "{0}".format(classes[int(x[-1])])
+
+                carbox = Polygon([(c1[0], c1[0]), (c1[0], c1[1]), (c1[1], c1[1]), (c1[1], c1[0])])
+                carcent = Point((centx, centy)) # Car Center point
+                carundcent = Point((centx, c2[1]))
+
+                """ 차의 중앙 지점과 겹치는 곳이 있으면 그곳이 차의 위치 """
+                for val in vals:
+                    if int(x[-1]) == val:
+                        cnt += 1
+                        if l_poly.intersects(carcent):
+                            l_cnt += 1
+                        if r_poly.intersects(carcent):
+                            r_cnt += 1
+                        if c_poly.intersects(carcent):
+                            c_cnt += 1
+                            if c_cnt > 1 : c_cnt = 1
+
+                            # 앞 차량과의 거리계산
+                            pl = carundcent.distance(Point(whalf-5, 720))
+                            dist = (pl * 1.8 / (next_frame[6] - next_frame[2])) * 180/np.pi
+                            dist = round(map(dist, 20, 40, 10, 70), 2)
+
+                            # 앞 차량의 Detection Box----------------------------
+                            cv2.rectangle(frame, c1, c2, blue, 1)
+
+                            t_size = cv2.getTextSize(label, font2, 1, 1)[0]
+                            c2 = c1[0] + t_size[0], c1[1] - t_size[1]
+
+                            cv2.rectangle(frame, c1, c2, blue, -1)
+                            #---------------------------------------------------
+
+                            cv2.line(frame, (centx, c1[1]), (centx, c1[1]-120), purple, 1)
+                            cv2.line(frame, (centx-50, c1[1]-120), (centx+40, c1[1]-120), purple, 1)
+                            cv2.putText(frame, "{} m".format(dist), (centx-45, c1[1]-130), font, 0.6, purple, 1)
+
+                        if l_cnt or r_cnt or c_cnt:
+                            cnt = l_cnt + c_cnt + r_cnt
+
+            cv2.putText(frame, 'vehicles counting : {}'.format(cnt), (10, 75), font, 0.8, white, 1)
+            cv2.putText(frame, 'L = {0} / F = {2} / R = {1}'.format(l_cnt, r_cnt, c_cnt), (10, 100), font, 0.7, white, font_size)
+
+            """ Result """
+            object_detection = cv2.add(frame, zero_frame)
+            lane_detection = cv2.addWeighted(object_detection, 1, lane_detection, 0.5, 0)
+            cv2.imshow("Result", lane_detection)
             # clip1.write(lane_result)
 
+        # lane_detection = cv2.addWeighted(frame, 1, lane_detection, 0.5, 0)
+        # cv2.imshow("hough", lane_detection)
         frames += 1
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
